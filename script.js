1967e4f327a75330dd9f54d970674f3e0caec596
Amirhossein Emadi <emadi.henker404@gmail.com>
2024-12-24 11:45:37 +0330

    Add functionality to show/hide alert and character table with results

--- script.js
+++ script.js
@@ -17,3 +17,51 @@ function checkRepeatedCharacter() {
   });
   console.log(listObject);
 }
+
+// show or hide alert and table:
+function alertOfResult(listOfCharacters = [], visible = true) {
+  const alert = document.getElementById("alertOfResult"),
+    tableOfCharacters = document.getElementById("tableOfCharacters"),
+    tbodyOfTable = tableOfCharacters.querySelector("table tbody");
+  tbodyOfTable.innerHTML = "";
+  if (!visible) {
+    alert.classList.add("d-none");
+    tableOfCharacters.classList.add("d-none");
+    return;
+  }
+  alert.classList.remove("d-none");
+  tableOfCharacters.classList.remove("d-none");
+  listOfCharacters.sort((a, b) => b["repeat"] - a["repeat"]);
+  let counter = 1;
+  listOfCharacters.map((char) => {
+    if (counter === 1) {
+      let message = char["character"];
+      if (char["repeat"] === 1) message = "Repeated character not found! (404)";
+      alert.querySelector("strong").innerHTML = message;
+    }
+    const tr = document.createElement("tr"),
+      th = document.createElement("th"),
+      tdChar = document.createElement("td"),
+      tdCount = document.createElement("td"),
+      tdAscii = document.createElement("td"),
+      tdCss = document.createElement("td"),
+      tdHtml = document.createElement("td");
+    th.scope = "row";
+    th.innerHTML = String(counter);
+    tdChar.innerHTML = char["character"];
+    tdCount.innerHTML = char["repeat"];
+    char["ascii"] === false
+      ? (tdAscii.innerHTML = "- - -")
+      : (tdAscii.innerHTML = char["ascii"]);
+    tdCss.innerHTML = char["css"];
+    tdHtml.innerHTML = char["html"];
+    tr.appendChild(th);
+    tr.appendChild(tdChar);
+    tr.appendChild(tdCount);
+    tr.appendChild(tdAscii);
+    tr.appendChild(tdCss);
+    tr.appendChild(tdHtml);
+    tableOfNumbers.querySelector("tbody").appendChild(tr);
+    counter++;
+  });
+}
